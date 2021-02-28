a06644e510b52160cef6e1e723c05782f4994274
baloh <balohbaloh@yahoo.co.id>
2021-02-28 13:40:24 +0700

    Implement Redux

--- src/index.js
+++ src/index.js
@@ -1,43 +1,29 @@
-import React, { useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import PeopleList from './components/PeopleList';
+import AddPersonForm from './components/AddPersonForm';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
 
-function AddPersonForm(props) {
-  const [ person, setPerson ] = useState("");
+const initialState = {
+  contacts : ["Matthew Bellamy", "Dominic Howard", "Chris Wolstenhome"]
+};
 
-  function handleChange(e) {
-    setPerson(e.target.value);
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_PERSON':
+      return {...state,
+        contacts: [...state.contacts, action.data]}
+    default:
+      return state;
   }
-
-  function handleSubmit(e) {
-    props.handleSubmit(person);
-    setPerson('');
-    e.preventDefault();
-  }
-  
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text"
-        placeholder="Add new contact"
-        onChange={handleChange}
-        value={person} />
-      <button type="submit">Add</button>
-    </form>
-  )
 }
 
-function PeopleList(props) {
-  const arr = props.data;
-  const listItems = arr.map((val, index) =>
-    <li key={index}>{val}</li>
-  );
-  return <ul>{listItems}</ul>
-}
+const store = createStore(reducer);
 
-const contacts = ["Matthew Bellamy", "Dominic Howard", "Chris Wolstenhome"];
-
-function ContactManager(props) {
+/* function ContactManager(props) {
   const[contacts, setContacts] = useState(props.data);
 
   function addPerson(name) {
@@ -50,9 +36,12 @@ function ContactManager(props) {
       <PeopleList data={contacts} />
     </div>
   );
-}
+} */
 
-ReactDOM.render(<ContactManager data={contacts} />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+  <AddPersonForm />
+  <PeopleList />
+</Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
